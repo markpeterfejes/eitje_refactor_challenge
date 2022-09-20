7746803468610f80ba1da5573d8ed644ecfde658
Mark Peter Fejes <markpeter@quantum-intermission.space>
2022-09-20 15:38:48 +0200

    feat: pure function version

--- content/my_solution.ts
+++ content/my_solution.ts
@@ -1,6 +1,6 @@
 import { argv } from "process";
 
-type Duration = {
+type DurationType = {
   years: number;
   days: number;
   hours: number;
@@ -8,8 +8,64 @@ type Duration = {
   seconds: number;
 };
 
-function formatDuration(duration: number | string): string {
-  return duration.toString();
+class Duration {
+  years: number;
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+  
+  constructor(second: number) {
+
+  }
 }
 
-console.log(formatDuration(argv[2]));
+const MINUTE_SECONDS = 60;
+const HOUR_SECONDS = MINUTE_SECONDS * 60;
+const DAYS_SECONDS = HOUR_SECONDS * 24;
+const YEAR_SECONDS = DAYS_SECONDS * 365;
+
+function parseDuration(durationInSeconds: number): Duration {
+  const years = Math.floor(durationInSeconds / YEAR_SECONDS);
+
+  const days = Math.floor(
+    (durationInSeconds - years * YEAR_SECONDS) / DAYS_SECONDS
+  );
+
+  const hours = Math.floor(
+    (durationInSeconds - (years * YEAR_SECONDS + days * DAYS_SECONDS)) /
+      HOUR_SECONDS
+  );
+
+  const minutes = Math.floor(
+    (durationInSeconds -
+      (years * YEAR_SECONDS + days * DAYS_SECONDS + hours * HOUR_SECONDS)) /
+      MINUTE_SECONDS
+  );
+
+  const seconds = Math.floor(
+    durationInSeconds -
+      (years * YEAR_SECONDS +
+        days * DAYS_SECONDS +
+        hours * HOUR_SECONDS +
+        minutes * MINUTE_SECONDS)
+  );
+
+  return {
+    years,
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+}
+
+function formatDuration(duration: number): string {
+  return JSON.stringify(parseDuration(duration), undefined, 4);
+}
+
+const inputDuration = parseInt(argv[2]);
+
+if (isNaN(inputDuration)) throw new TypeError("Please only provide numbers");
+
+console.log(formatDuration(inputDuration));
